9ed029c129dbc8112d01f5e8e567714444b3710d
Mesutcan Yediveren <mesutcanyediveren@hotmail.com>
2023-01-12 14:45:02 +0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} style={{transform: `rotate(${counter * 40}deg)`}} className="App-logo"alt="logo" />
          <div style={{fontSize: "40px"}}>{counter}</div>
          <div>
             <button onClick={() => setCounter(counter-1)}>-</button>
