5b3a9aff6c8ec8851e93bb71e69c1f135889ecf1
Mesutcan Yediveren <mesutcanyediveren@hotmail.com>
2023-01-12 14:07:04 +0300

    add gh-pages scripts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <div style={{fontSize: "40px"}}>{counter}</div>
+         <div>
+            <button onClick={() => setCounter(counter-1)}>-</button>
+            <button onClick={() => setCounter(counter+1)}>+</button>
+         </div>
       </header>
     </div>
   );
